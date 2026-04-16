c3a46429debd918be0b53ddb8076630b26b060b5
@romel.ante8250 <romel.ante8250@utc.edu.ec>
2026-04-15 20:46:04 -0500

    Add core application structure with models, views, and templates; implement home page and URL routing

--- Applications/core/models.py
+++ Applications/core/models.py
@@ -1,17 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-# Create your models here.
-'''class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=200)
-    phone = models.CharField(max_length=20, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name'''
         
 class Profile(models.Model):
     id = models.AutoField(primary_key=True)
